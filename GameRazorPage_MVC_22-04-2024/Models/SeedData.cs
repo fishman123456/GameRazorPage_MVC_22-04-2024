d74caa4270d1a15b3d6243dc7a70c3fc067af536
fishman123456 <den9152@yandex.ru>
2024-04-23 01:21:41 +0700

    добавляет данные если таблица пустая

---
+++ GameRazorPage_MVC_22-04-2024/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using GameRazorPage_MVC_22_04_2024.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameRazorPage_MVC_22_04_2024.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new GameRazorPage_MVC_22_04_2024Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<GameRazorPage_MVC_22_04_2024Context>>()))
+            {
+                if (context == null || context.VideoGame == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.VideoGame.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.VideoGame.AddRange(
+                    new VideoGame
+                    {
+                        Title = "When Harry Met Sally",
+                        Description = "abpi",
+                        Price = 45,
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Rate = 2,
+                        CopiesSold = 2
+                    }
+                ); ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- GameRazorPage_MVC_22-04-2024/Program.cs
+++ GameRazorPage_MVC_22-04-2024/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using GameRazorPage_MVC_22_04_2024.Data;
+using GameRazorPage_MVC_22_04_2024.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +10,12 @@ builder.Services.AddDbContext<GameRazorPage_MVC_22_04_2024Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GameRazorPage_MVC_22_04_2024Context") ?? throw new InvalidOperationException("Connection string 'GameRazorPage_MVC_22_04_2024Context' not found.")));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
